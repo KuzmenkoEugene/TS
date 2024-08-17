c57b5bc6b2c2b722257cd1692836f28c1c997c54
Eugene <evgenkuzmenko94@gmail.com>
2024-08-17 23:20:24 +0300

    changed HW

--- 12.1/HW12.1/index.js
+++ 12.1/HW12.1/index.js
@@ -1,89 +1,49 @@
-var moviesValue = [
-    {
-        name: "The Shawshank Redemption",
-        year: 1994,
-        rating: 9.3,
-        awards: 21,
-    },
-    {
-        name: "Pulp Fiction",
-        year: 1994,
-        rating: 8.9,
-        awards: 43,
-    },
-    {
-        name: "The Lord of the Rings: The Return of the King",
-        year: 2003,
-        rating: 8.9,
-        awards: 189,
-    },
-    {
-        name: "The Dark Knight Rises",
-        year: 2012,
-        rating: 8.4,
-        awards: 46,
-    },
-    {
-        name: "Harry Potter and the Prisoner of Azkaban",
-        year: 2004,
-        rating: 7.9,
-        awards: 42,
-    },
-    {
-        name: "Dune",
-        year: 2021,
-        rating: 8.0,
-        awards: 35,
-    },
-];
-var categories = [
-    {
-        name: "thriller",
-        movies: [
-            {
-                name: "Se7en",
-                year: 1995,
-                rating: 8.6,
-                awards: 34,
-            },
-            {
-                name: "Zodiac",
-                year: 2007,
-                rating: 7.7,
-                awards: 71,
-            },
-        ],
-    },
-    {
-        name: "comedy",
-        movies: [
-            {
-                name: "Superbad",
-                year: 2007,
-                rating: 7.6,
-                awards: 19,
-            },
-            {
-                name: "The Grand Budapest Hotel",
-                year: 2014,
-                rating: 8.1,
-                awards: 135,
-            },
-        ],
-    },
-];
-function filmGenreMovie(categories, nameCategory) {
-    return categories.filter(function (el) { return el.name === nameCategory; });
-}
-function searchRangeMovies(movies, searchValue, filterFrom, filterTo) {
-    return movies.filter(function (el) {
-        var value = el[searchValue];
-        if (typeof value === "number") {
-            return value >= filterFrom && value <= filterTo;
-        }
-        return false;
-    });
-}
-function nameMovie(movies, values) {
-    return movies.filter(function (el) { return el.name === values; });
-}
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var GridFilterTypeEnum;
+(function (GridFilterTypeEnum) {
+    GridFilterTypeEnum["NAME"] = "NAME";
+    GridFilterTypeEnum["FROM_TO"] = "FROM_TO";
+    GridFilterTypeEnum["VALUE_SEARCH"] = "VALUE_SEARCH";
+})(GridFilterTypeEnum || (GridFilterTypeEnum = {}));
+var Filters = /** @class */ (function () {
+    function Filters() {
+        this.filters = [];
+        this.searchValues = [];
+    }
+    Filters.prototype.applyFiltersValue = function (filters) {
+        this.filters = filters;
+    };
+    Filters.prototype.applySearchValue = function (searchValues) {
+        this.searchValues = searchValues;
+    };
+    return Filters;
+}());
+var MoviesList = /** @class */ (function (_super) {
+    __extends(MoviesList, _super);
+    function MoviesList(movies) {
+        var _this = _super.call(this) || this;
+        _this.movies = movies;
+        return _this;
+    }
+    MoviesList.prototype.search = function () {
+        return this.movies;
+    };
+    MoviesList.prototype.filter = function () {
+        return this.movies;
+    };
+    return MoviesList;
+}(Filters));
--- 12.1/HW12.1/index.ts
+++ 12.1/HW12.1/index.ts
@@ -1,115 +1,61 @@
 interface IMovie {
-  name: string;
-  year: number;
-  rating: number;
-  awards: number;
+  name: string;  
+  year: number;  
+  rating: number; 
+  awards: number; 
 }
 
-interface ICategory {
-  name: string;
+interface Category {
+  name: string;   
   movies: IMovie[];
 }
 
-let moviesValue: IMovie[] = [
-  {
-    name: "The Shawshank Redemption",
-    year: 1994,
-    rating: 9.3,
-    awards: 21,
-  },
-  {
-    name: "Pulp Fiction",
-    year: 1994,
-    rating: 8.9,
-    awards: 43,
-  },
-  {
-    name: "The Lord of the Rings: The Return of the King",
-    year: 2003,
-    rating: 8.9,
-    awards: 189,
-  },
-  {
-    name: "The Dark Knight Rises",
-    year: 2012,
-    rating: 8.4,
-    awards: 46,
-  },
-  {
-    name: "Harry Potter and the Prisoner of Azkaban",
-    year: 2004,
-    rating: 7.9,
-    awards: 42,
-  },
-  {
-    name: "Dune",
-    year: 2021,
-    rating: 8.0,
-    awards: 35,
-  },
-];
-
-let categories: ICategory[] = [
-  {
-    name: "thriller",
-    movies: [
-      {
-        name: "Se7en",
-        year: 1995,
-        rating: 8.6,
-        awards: 34,
-      },
-      {
-        name: "Zodiac",
-        year: 2007,
-        rating: 7.7,
-        awards: 71,
-      },
-    ],
-  },
-  {
-    name: "comedy",
-    movies: [
-      {
-        name: "Superbad",
-        year: 2007,
-        rating: 7.6,
-        awards: 19,
-      },
-      {
-        name: "The Grand Budapest Hotel",
-        year: 2014,
-        rating: 8.1,
-        awards: 135,
-      },
-    ],
-  },
-];
-
-function filmGenreMovie(
-  categories: ICategory[],
-  nameCategory: string
-): ICategory[] {
-  return categories.filter((el) => el.name === nameCategory);
+enum GridFilterTypeEnum {
+  NAME = 'NAME',       
+  FROM_TO = 'FROM_TO', 
+  VALUE_SEARCH = 'VALUE_SEARCH', 
 }
 
-function searchRangeMovies(
-  movies: IMovie[],
-  searchValue: keyof IMovie,
-  filterFrom: number,
-  filterTo: number
-): IMovie[] {
-  return movies.filter((el) => {
-    const value = el[searchValue];
+type GridFilterValue<T> = {
+  type: GridFilterTypeEnum; 
+  filter: Extract<T, string | number>; 
+  filterTo?: Extract<T, string | number>; 
+};
 
-    if (typeof value === "number") {
-      return value >= filterFrom && value <= filterTo;
-    }
+type GridFilterSetValues<T> = {
+  values: T[];
+};
 
-    return false;
-  });
+abstract class Filters<T> {
+  protected filters: GridFilterValue<any>[] = [];
+  protected searchValues: GridFilterSetValues<string>[] = []; 
+
+  applyFiltersValue(filters: GridFilterValue<any>[]): void {
+    this.filters = filters; 
+  }
+
+  applySearchValue(searchValues: GridFilterSetValues<string>[]): void {
+    this.searchValues = searchValues; 
+  }
+
+  abstract search(): T[]; 
+  abstract filter(): T[]; 
 }
 
-function nameMovie(movies: IMovie[], values: string): IMovie[] {
-  return movies.filter((el) => el.name === values);
+class MoviesList extends Filters<IMovie> {
+  public movies: IMovie[]; 
+
+  constructor(movies: IMovie[]) {
+    super();
+    this.movies = movies; 
+  }
+
+  search(): IMovie[] {
+    return this.movies;
+  }
+
+  filter(): IMovie[] {
+
+    return this.movies;
+  }
 }
